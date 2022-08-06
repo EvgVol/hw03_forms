66b5512acf0c41861bd8ec97447322a4e9eb8005
Evgeniy Volochek <volochek93@yandex.ru>
2022-08-06 16:14:00 +0300

    pep8

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -10,7 +10,7 @@ def index(request):
     """Описывает работу главной страницы."""
     post_list = Post.objects.select_related('author').all()
     context = {'page_obj': paginator_posts(request, post_list)}
-    return render(request,'posts/index.html', context)
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
@@ -40,7 +40,7 @@ def profile(request, username):
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    return render(request, 'posts/post_detail.html',{'post': post})
+    return render(request, 'posts/post_detail.html', {'post': post})
 
 
 @login_required
